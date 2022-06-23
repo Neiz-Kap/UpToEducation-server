13f879df8133f023e31fd47754dfe341a0321d93
Neiz-Kap <ivan.mehedoff@yandex.ru>
2022-06-23 18:45:54 +0300

    try fix heroku deploy

--- db.js
+++ db.js
@@ -1,22 +1,15 @@
 const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize(
-  process.env.POSTGRES_NAME,
-  process.env.POSTGRES_USER,
-  process.env.POSTGRES_PASSWORD,
-  {
-    dialect: "postgres",
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+module.exports = new Sequelize(process.env.DB_LINK, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-  }
-);
+  },
+});
 
 module.exports
   .authenticate()
